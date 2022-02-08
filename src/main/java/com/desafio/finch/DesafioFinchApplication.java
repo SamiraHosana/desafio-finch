e4ad0474898cd38558e5ae3e7016459a6b656bcb
SamiraHosana <samirahosana0710@gmail.com>
2022-02-08 20:41:51 -0300

    commit inicial

---
+++ src/main/java/com/desafio/finch/DesafioFinchApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.finch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFinchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/finch/DesafioFinchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.finch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
